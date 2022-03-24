7546f1adfe67281891eb070089b2bcc1f6f99e25
MonsterEater <35862599+MonsterEater@users.noreply.github.com>
2022-03-24 14:49:47 +0800

    Add files via upload

---
+++ Chapter 3 Programming Exercise/c3e3.c
@@ -0,0 +1,13 @@
+//program sounds an alert and print
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("\a\n");
+	printf("Startled by the sudden soud, Sally shouted,\n");
+	printf("\"By the Great Pumpkin, what was that!\"\n");
+
+	return 0;
+}
+
